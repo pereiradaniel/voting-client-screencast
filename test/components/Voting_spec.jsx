b115d9e7c75ce57882818cd08fa9ae964bb6cfba
Daniel <truenorthtrainingcompany@gmail.com>
2016-12-08 15:36:24 -0500

    Troubleshooting

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -5,7 +5,6 @@ import {
   scryRenderedDOMComponentsWithTag,
   Simulate
 } from 'react-addons-test-utils';
-import {List} from 'immutable';
 import {Voting} from '../../src/components/Voting';
 import {expect} from 'chai';
 
